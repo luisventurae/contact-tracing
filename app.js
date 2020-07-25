2bbfca70b3b901b439d6a54edbaeb0036d36b6e3
Luis Ventura <luisigsystem@gmail.com>
2020-07-25 16:12:35 -0500

    [updated] prepare main js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('[inicio]')
+'use strict'
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 12345
+const SVR = process.env.SVR || 'localhost'
+
+app.listen(PORT, () => {
+    console.log(`http://${SVR}:${PORT}`)
+})
